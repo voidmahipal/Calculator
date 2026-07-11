7de82e61ec71e3cf593c8bcd81f35f7faa83bd16
voidmahipal <mahipal7131@gmail.com>
2026-07-11 23:47:26 +0530

    Fixed clear and equal button and added JS file with four basic functions

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function add(num1,num2) {
+    return num1+num2;
+}
+function sub(num1,num2) {
+    return num1-num2;
+}
+function multiply(num1,num2) {
+    return num1*num2;
+}
+function divide(num1,num2) {
+    if(!num2) return "Chill out bro. Don't divide by 0!!";
+    return num1/num2;
+}
+let a;
+let b;
+let op;
